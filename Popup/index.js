b1d2058c1cafd699c2cd287193f781c9b666a8ef
Grant Rynders <grantrynders@outlook.com>
2024-06-04 09:19:26 -0400

    Rudimentary timer and id changes

--- Popup/index.js
+++ Popup/index.js
@@ -1,4 +1,31 @@
 // This javascript will affect the index page
 function onButtonClick() {
-    alert('Button clicked!');
+    timer()
+}
+function timer(){
+    var sec = 0;
+    var min = 0;
+    var secString;
+    var minString;
+    var timer = setInterval(function(){
+        
+        sec++;
+        while (sec > 60)
+        {
+            sec - 60
+            min + 1
+        }
+        if (min < 10)
+        {
+            minString = "0" + min
+        }
+        if (sec < 10)
+        {
+            secString = "0" + sec
+        }
+        document.getElementById('timerDisplay').innerHTML= minString + ':' + secString;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
 }
