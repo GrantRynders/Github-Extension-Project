63be3a1faac3bd2e55804376fb2e5fa99926948f
Grant Rynders <grantrynders@outlook.com>
2024-06-11 14:14:42 -0400

    More prisma setup

--- server.js
+++ server.js
@@ -3,8 +3,31 @@ const app = express();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 app.use(express.json());
+
+
+
+//ENDPOINT EXAMPLE
 app.get("/user", async (req, res) => {
     const returnedUsers = await prisma.user.findMany();
     res.json(returnedUsers);
 });
 app.listen(3001, () => console.log('Server running on port ${3001}'));
+
+
+//CREATE USER EXAMPLE
+const user = await prisma.user.create({
+    data: {
+      UserName: 'JohnSmith',
+    },
+  })
+
+//
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
