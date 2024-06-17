665bb1733a9ee4c2dacd988fb919dbca79d97df8
Grant Rynders <grantrynders@outlook.com>
2024-06-17 14:45:49 -0400

    Issues endpoint for user page

--- myExpressApp/pages/objectmodels/scripts/user.js
+++ myExpressApp/pages/objectmodels/scripts/user.js
@@ -36,7 +36,7 @@ async function GetData()
     return returnData;
 }
 
-async function DrawUserTimeSpentGraph() 
+async function DrawUserTimeSpentGraph(userId) 
 {
   const chartData = await fetch("http://localhost:5220/usermodel/" + userId + "/timespent", {
     method: "GET", // *GET, POST, PUT, DELETE, etc.
@@ -59,11 +59,45 @@ async function DrawUserTimeSpentGraph()
     {
       type: 'bar',
       data: {
-        labels: data.map(row => row.year),
+        labels: chartData.map(row => row.year),
         datasets: [
           {
             label: 'Time spent by user by period',
-            data: data.map(row => row.count)
+            data: chartData.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
+}
+async function DrawUserIssuesGraph(userId) 
+{
+  const chartData = await fetch("/usermodel/" + userId + "/issues", {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer",
+})
+  .catch( function() {
+        console.log("Unable to fetch user graph data");
+  });
+
+  nnew Chart(
+    document.getElementById('acquisitions'),
+    {
+      type: 'bar',
+      data: {
+        labels: chartData.map(row => row.year),
+        datasets: [
+          {
+            label: 'Time spent by user by period',
+            data: chartData.map(row => row.count)
           }
         ]
       }
