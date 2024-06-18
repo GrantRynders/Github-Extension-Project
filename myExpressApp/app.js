2279ae1a68368d0b45a7b23ea513e3f9314ad677
Grant Rynders <grantrynders@outlook.com>
2024-06-18 15:29:31 -0400

    Updated server site user interface

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -249,6 +249,10 @@ app.get('^/$|/test(.html)?', (req, res) => {
   //res.sendFile('./views/index.html', { root: __dirname });
   res.sendFile(path.join(__dirname, 'views', 'test.html'));
 });
+app.get('^/$|/main(.html)?', (req, res) => {
+  //res.sendFile('./views/index.html', { root: __dirname });
+  res.sendFile(path.join(__dirname, 'views', 'main.html'));
+});
 app.get('^/$|/views/users(.html)?', (req, res) => {
   //res.sendFile('./views/index.html', { root: __dirname });
   res.sendFile(path.join(__dirname, 'views', 'users.html'));
