5d2e79964f5ff8a312cf7a0b18b2d84817a3045e
Grant Rynders <grantrynders@outlook.com>
2024-06-21 15:27:23 -0400

    Removed unnecessary value

--- myExpressApp/routes/user.js
+++ myExpressApp/routes/user.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    res.render('user', { title: 'Steve' });
+    res.render('user', { title: 'User' });
 });
 
 module.exports = router;
