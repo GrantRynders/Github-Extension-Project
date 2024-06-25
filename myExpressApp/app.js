bee39ecbcc26e44e6db5d359e15784b27bffe5bf
Grant Rynders <grantrynders@outlook.com>
2024-06-25 13:27:28 -0400

    Removed irrelevant files/folders

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({ origin: '*' }));
 
 app.get("/views/usermodel/user/:id", async (req, res) => {
