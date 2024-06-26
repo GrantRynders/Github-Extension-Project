cac539bf2ce55a2aa91cba762958044bcda67d06
deshyah <deshyahanderson@gmail.com>
2024-06-26 10:55:22 -0400

    yay

--- myExpressApp/prisma/index.ts
+++ myExpressApp/prisma/index.ts
@@ -1,17 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient()  
 
 async function main() {
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  
 }
 
 main()
-  .catch(e => {
-    console.error(e.message)
-  })
-  .finally(async () => {
+  .then(async () => {
     await prisma.$disconnect()
-
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
   })
