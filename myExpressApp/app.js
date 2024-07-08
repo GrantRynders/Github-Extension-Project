5bd768df0dea4c20017550d2e9f0b28862d48c60
Grant Rynders <grantrynders@outlook.com>
2024-07-08 10:16:25 -0400

    Universal Style Sheet thank goodness

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -13,6 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use('/favicon.ico', express.static('./favicon.ico'));
+app.use('/style.css', express.static('./views/style.css'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
